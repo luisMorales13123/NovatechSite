ee2c9a9d13bc8e5fefd122510c54a4462795076b
luisMorales13123 <93411473+luisMorales13123@users.noreply.github.com>
2024-06-15 14:31:17 -0500

    Delete Server directory

--- Server/index.ts
+++
@@ -1,8 +0,0 @@
-import ServerEquipo from "./modulos/server";
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const server = new ServerEquipo();
-
-
--- Server/rutas/User.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from'express'
-import { loginUser, newUser } from '../controlador/User/User';
-
-const router = Router();
-
-router.post('/',newUser);
-router.post('/login' ,loginUser);
-
-export default router;
